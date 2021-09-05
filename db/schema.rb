bdd470fa166c67be48eca004647323c3376945d0
Court McFadzean <court@kajabi.com>
2021-09-05 10:55:55 -0700

    adding invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_160321) do
+ActiveRecord::Schema.define(version: 2021_09_05_162401) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(version: 2021_09_05_160321) do
     t.index ["user_id"], name: "index_clients_on_user_id"
   end
 
+  create_table "invoices", force: :cascade do |t|
+    t.string "title"
+    t.string "number"
+    t.date "date"
+    t.date "due"
+    t.text "notes"
+    t.integer "user_id", null: false
+    t.integer "client_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_id"], name: "index_invoices_on_client_id"
+    t.index ["user_id"], name: "index_invoices_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_09_05_160321) do
   end
 
   add_foreign_key "clients", "users"
+  add_foreign_key "invoices", "clients"
+  add_foreign_key "invoices", "users"
 end
